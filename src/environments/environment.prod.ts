fcca017d994ab2a96d768ece4c90511ab8504e32
Dhanashri <kolidhanashri05@gmail.com>
2026-04-14 23:07:00 +0530

    build fixes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,14 @@
 export const environment = {
   production: true,
   googleFormUrl: 'https://docs.google.com/forms/d/e/1FAIpQLSdy8sQOPxUnK-9L29eF8tEJHoghIa1Ew0SX2qnh98hEZN1HTw/formResponse',
-  googleSheetScriptUrl: 'https://script.google.com/macros/s/AKfycbyMN6vKfJ6YDNHEMPz-XJiWs4gd6GbnmXMBsJAEeW2_7A7za6Ajo_AWrU+XzMdHZNiiXA/exec', // Sample but keep same if not available
+  // googleSheetScriptUrl: 'https://script.google.com/macros/s/AKfycbyMN6vKfJ6YDNHEMPz-XJiWs4gd6GbnmXMBsJAEeW2_7A7za6Ajo_AWrUqXzMdHZNiiXA/exec',
+  googleSheetScriptUrl: 'https://script.google.com/macros/s/AKfycbw_2KL54AFUvFZbTPKYBVCuA5ldC4RL4mM2wZ-MNvwLHqGiBXa7NXR_t5KTenS7uw5s/exec',
   entryIds: {
     name: 'entry.694821300',
-    email: 'entry.1208457199',
+    city: 'entry.1149235169',
     mobile: 'entry.789081934',
-    project: 'entry.529427854',
-    billAmount: 'entry.1570199244'
+    email: 'entry.529427854',
+    project: 'entry.1570199244',
+    billAmount: 'entry.660365310'
   }
 };
